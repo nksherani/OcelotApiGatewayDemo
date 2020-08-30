d517a7759e4359f13714ef640af5f859e2a4a0f6
naveed <nksherani@outlook.com>
2020-08-30 15:01:04 +0500

    Add project files.

---
+++ src/ProductsService/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProductsService.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Cream", "Shampoo", "Cooking Oil", "Ice Cream"
+        };
+
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Summaries;
+        }
+    }
+}
---
+++ src/UsersService/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CatalogService.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "David", "John", "Bravo"
+        };
+
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Summaries;
+        }
+    }
+}
